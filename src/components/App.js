25b4e0a29094891bc9e723a6b3a5ee6648bcc18d
yussufhh <0793477632g@gmail.com>
2024-06-22 14:50:26 +0300

    complete-react-hooks-components-basics-lab

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,14 @@ function Home() {
   );
 }
 
+function About() {
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  );
+}
+
 {/* write an <About> component here */}
 
 
@@ -23,8 +31,10 @@ function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+   
+      <Home />
+      <About />
+     
     </div>
   );
 }
